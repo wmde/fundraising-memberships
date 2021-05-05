aa7058473fa046acefb2208720223a6c055b7200
Abban Dunne <abban.dunne@wikimedia.de>
2021-05-05 09:12:07 +0200

    Add logger to ModerateMembershpApplicationUseCase

--- src/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCase.php
+++ src/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCase.php
@@ -5,13 +5,19 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\MembershipContext\UseCases\ModerateMembershipApplication;
 
 use WMDE\Fundraising\MembershipContext\Domain\Repositories\ApplicationRepository;
+use WMDE\Fundraising\MembershipContext\Infrastructure\MembershipApplicationEventLogger;
 
 class ModerateMembershipApplicationUseCase {
 
-	private ApplicationRepository $applicationRepository;
+	public const LOG_MESSAGE_MARKED_FOR_MODERATION = 'marked for moderation by user: %s';
+	public const LOG_MESSAGE_MARKED_AS_APPROVED = 'marked as approved by user: %s';
 
-	public function __construct( ApplicationRepository $applicationRepository ) {
+	private ApplicationRepository $applicationRepository;
+	private MembershipApplicationEventLogger $applicationEventLogger;
+
+	public function __construct( ApplicationRepository $applicationRepository, MembershipApplicationEventLogger $applicationEventLogger ) {
 		$this->applicationRepository = $applicationRepository;
+		$this->applicationEventLogger = $applicationEventLogger;
 	}
 
 	public function markMembershipApplicationAsModerated( int $membershipApplicationId, string $authorizedUser ): ModerateMembershipApplicationResponse {
@@ -32,6 +38,11 @@ class ModerateMembershipApplicationUseCase {
 		$membershipApplication->markForModeration();
 		$this->applicationRepository->storeApplication( $membershipApplication );
 
+		$this->applicationEventLogger->log(
+			$membershipApplicationId,
+			sprintf( self::LOG_MESSAGE_MARKED_FOR_MODERATION, $authorizedUser )
+		);
+
 		return $this->newModerationSuccessResponse( $membershipApplicationId );
 	}
 
@@ -48,6 +59,11 @@ class ModerateMembershipApplicationUseCase {
 
 		$membershipApplication->approve();
 		$this->applicationRepository->storeApplication( $membershipApplication );
+
+		$this->applicationEventLogger->log(
+			$membershipApplicationId,
+			sprintf( self::LOG_MESSAGE_MARKED_AS_APPROVED, $authorizedUser )
+		);
 
 		return $this->newModerationSuccessResponse( $membershipApplicationId );
 	}
--- tests/Unit/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCaseTest.php
+++ tests/Unit/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCaseTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 use WMDE\Fundraising\MembershipContext\Domain\Model\MembershipApplication;
 use WMDE\Fundraising\MembershipContext\Tests\Data\ValidMembershipApplication;
 use WMDE\Fundraising\MembershipContext\Tests\Fixtures\FakeApplicationRepository;
+use WMDE\Fundraising\MembershipContext\Tests\Fixtures\MembershipApplicationEventLoggerSpy;
 use WMDE\Fundraising\MembershipContext\UseCases\ModerateMembershipApplication\ModerateMembershipApplicationUseCase;
 
 /**
@@ -17,6 +18,15 @@ use WMDE\Fundraising\MembershipContext\UseCases\ModerateMembershipApplication\Mo
 class ModerateMembershipApplicationUseCaseTest extends TestCase {
 
 	private const AUTH_USER_NAME = "Pintman Paddy Losty";
+
+	private MembershipApplicationEventLoggerSpy $membershipApplicationEventLogger;
+	private FakeApplicationRepository $applicationRepository;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->membershipApplicationEventLogger = new MembershipApplicationEventLoggerSpy();
+		$this->applicationRepository = new FakeApplicationRepository();
+	}
 
 	public function testModerateNonExistentMembershipApplication_actionFails() {
 		$useCase = $this->newUseCase();
@@ -32,6 +42,15 @@ class ModerateMembershipApplicationUseCaseTest extends TestCase {
 		$response = $useCase->approveMembershipApplication( 1, self::AUTH_USER_NAME );
 
 		$this->assertFalse( $response->moderationChangeSucceeded() );
+	}
+
+	public function testSetModerateOnMembershipApplication_actionSucceeds() {
+		$application = ValidMembershipApplication::newDomainEntity();
+		$useCase = $this->newUseCase( $application );
+
+		$response = $useCase->markMembershipApplicationAsModerated( 1, self::AUTH_USER_NAME );
+
+		$this->assertTrue( $response->moderationChangeSucceeded() );
 	}
 
 	public function testSetModerateOnMembershipApplication_setsModerated() {
@@ -72,14 +91,14 @@ class ModerateMembershipApplicationUseCaseTest extends TestCase {
 		$this->assertFalse( $response->moderationChangeSucceeded() );
 	}
 
-	public function testApproveMembershipApplication_removesModerated() {
+	public function testApproveMembershipApplication_removesModeratedStatus() {
 		$application = ValidMembershipApplication::newDomainEntity();
 		$application->markForModeration();
 		$useCase = $this->newUseCase( $application );
 
-		$response = $useCase->approveMembershipApplication( 1, self::AUTH_USER_NAME );
+		$useCase->approveMembershipApplication( 1, self::AUTH_USER_NAME );
 
-		$this->assertTrue( $response->moderationChangeSucceeded() );
+		$this->assertFalse( $this->applicationRepository->getApplicationById( 1 )->needsModeration() );
 	}
 
 	public function testApproveOnApprovedMembershipApplication_actionFails() {
@@ -92,7 +111,41 @@ class ModerateMembershipApplicationUseCaseTest extends TestCase {
 		$this->assertFalse( $response->moderationChangeSucceeded() );
 	}
 
+	public function testOnModerateMembershipApplication_adminUserNameIsWrittenAsLogEntry(): void {
+		$application = ValidMembershipApplication::newDomainEntity();
+		$useCase = $this->newUseCase( $application );
+
+		$useCase->markMembershipApplicationAsModerated( 1, self::AUTH_USER_NAME );
+		$logs = $this->membershipApplicationEventLogger->getLogs();
+
+		$message = sprintf( ModerateMembershipApplicationUseCase::LOG_MESSAGE_MARKED_FOR_MODERATION, self::AUTH_USER_NAME );
+
+		$this->assertCount( 1, $logs );
+		$this->assertContains( $message, $logs );
+	}
+
+	public function testOnApproveMembershipApplication_adminUserNameIsWrittenAsLogEntry(): void {
+		$application = ValidMembershipApplication::newDomainEntity();
+		$application->markForModeration();
+		$useCase = $this->newUseCase( $application );
+
+		$useCase->approveMembershipApplication( 1, self::AUTH_USER_NAME );
+		$logs = $this->membershipApplicationEventLogger->getLogs();
+
+		$message = sprintf( ModerateMembershipApplicationUseCase::LOG_MESSAGE_MARKED_AS_APPROVED, self::AUTH_USER_NAME );
+
+		$this->assertCount( 1, $logs );
+		$this->assertContains( $message, $logs );
+	}
+
 	private function newUseCase( MembershipApplication ...$applications ): ModerateMembershipApplicationUseCase {
-		return new ModerateMembershipApplicationUseCase( new FakeApplicationRepository( ...$applications ) );
+		foreach ( $applications as $application ) {
+			$this->applicationRepository->storeApplication( $application );
+		}
+
+		return new ModerateMembershipApplicationUseCase(
+			$this->applicationRepository,
+			$this->membershipApplicationEventLogger
+		);
 	}
 }
