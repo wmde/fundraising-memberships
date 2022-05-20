dea9b176cede2f1f59bf3dd1d3901fa7ac1aa0d9
Abban Dunne <abban.dunne@wikimedia.de>
2022-05-20 15:14:14 +0200

    Update DomainToLegacyConverter
    * Add payment_id column to application table
    * Activate DomainToLegacyConverter tests
    * Remove payment data from entities
    
    Ticket: https://phabricator.wikimedia.org/T305051

--- src/DataAccess/LegacyConverters/DomainToLegacyConverter.php
+++ src/DataAccess/LegacyConverters/DomainToLegacyConverter.php
@@ -6,20 +6,22 @@ namespace WMDE\Fundraising\MembershipContext\DataAccess\LegacyConverters;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use WMDE\Euro\Euro;
 use WMDE\Fundraising\MembershipContext\DataAccess\DoctrineEntities\MembershipApplication as DoctrineApplication;
 use WMDE\Fundraising\MembershipContext\DataAccess\MembershipApplicationData;
 use WMDE\Fundraising\MembershipContext\Domain\Model\Applicant;
 use WMDE\Fundraising\MembershipContext\Domain\Model\MembershipApplication;
-use WMDE\Fundraising\PaymentContext\Domain\Model\DirectDebitPayment;
-use WMDE\Fundraising\PaymentContext\Domain\Model\PayPalPayment;
+use WMDE\Fundraising\PaymentContext\Domain\Model\LegacyPaymentData;
 
 class DomainToLegacyConverter {
-	public function convert( DoctrineApplication $doctrineApplication, MembershipApplication $application ): void {
+
+	public function convert( DoctrineApplication $doctrineApplication, MembershipApplication $application, LegacyPaymentData $paymentData ): void {
 		$doctrineApplication->setId( $application->getId() );
 		$doctrineApplication->setMembershipType( $application->getType() );
+		$doctrineApplication->setPaymentId( $application->getPaymentId() );
 
 		$this->setApplicantFields( $doctrineApplication, $application->getApplicant() );
-		$this->setPaymentFields( $doctrineApplication, $application->getPayment() );
+		$this->setPaymentFields( $doctrineApplication, $paymentData );
 		$doctrineApplication->setIncentives( $this->convertIncentives( $application->getIncentives() ) );
 		$doctrineApplication->setDonationReceipt( $application->getDonationReceipt() );
 
@@ -56,51 +58,27 @@ class DomainToLegacyConverter {
 		$application->setAddress( $address->getStreetAddress() );
 	}
 
-	private function setPaymentFields( DoctrineApplication $application, Payment $payment ): void {
-		$application->setPaymentIntervalInMonths( $payment->getIntervalInMonths() );
-		$application->setPaymentAmount( (int)$payment->getAmount()->getEuroFloat() );
-		$paymentMethod = $payment->getPaymentMethod();
-
-		$application->setPaymentType( $paymentMethod->getId() );
-		if ( $paymentMethod instanceof DirectDebitPayment ) {
-			$this->setBankDataFields( $application, $paymentMethod->getBankData() );
-		} elseif ( $paymentMethod instanceof PayPalPayment && $paymentMethod->getPayPalData() != new PayPalData() ) {
-			$this->setPayPalDataFields( $application, $paymentMethod->getPayPalData() );
+	private function setPaymentFields( DoctrineApplication $application, LegacyPaymentData $paymentdata ): void {
+		$application->setPaymentIntervalInMonths( $paymentdata->intervalInMonths );
+		$application->setPaymentAmount( Euro::newFromCents( $paymentdata->amountInEuroCents )->getEuros() );
+		if ( !empty( $paymentdata->paymentSpecificValues['iban'] ) ) {
+			$this->setBankDataFields( $application, $paymentdata->paymentSpecificValues );
+		} else {
+			$application->encodeAndSetData(
+				array_merge(
+					$application->getDecodedData(),
+					$paymentdata->paymentSpecificValues
+				)
+			);
 		}
 	}
 
-	private function setBankDataFields( DoctrineApplication $application, BankData $bankData ): void {
-		$application->setPaymentBankAccount( $bankData->getAccount() );
-		$application->setPaymentBankCode( $bankData->getBankCode() );
-		$application->setPaymentBankName( $bankData->getBankName() );
-		$application->setPaymentBic( $bankData->getBic() );
-		$application->setPaymentIban( $bankData->getIban()->toString() );
-	}
-
-	private function setPayPalDataFields( DoctrineApplication $application, PayPalData $payPalData ): void {
-		$application->encodeAndSetData( array_merge(
-			$application->getDecodedData(),
-			[
-				'paypal_payer_id' => $payPalData->getPayerId(),
-				'paypal_subscr_id' => $payPalData->getSubscriberId(),
-				'paypal_payer_status' => $payPalData->getPayerStatus(),
-				'paypal_address_status' => $payPalData->getAddressStatus(),
-				'paypal_mc_gross' => $payPalData->getAmount()->getEuroString(),
-				'paypal_mc_currency' => $payPalData->getCurrencyCode(),
-				'paypal_mc_fee' => $payPalData->getFee()->getEuroString(),
-				'paypal_settle_amount' => $payPalData->getSettleAmount()->getEuroString(),
-				'paypal_first_name' => $payPalData->getFirstName(),
-				'paypal_last_name' => $payPalData->getLastName(),
-				'paypal_address_name' => $payPalData->getAddressName(),
-				'ext_payment_id' => $payPalData->getPaymentId(),
-				'ext_subscr_id' => $payPalData->getSubscriberId(),
-				'ext_payment_type' => $payPalData->getPaymentType(),
-				'ext_payment_status' => $payPalData->getPaymentStatus(),
-				'ext_payment_account' => $payPalData->getPayerId(),
-				'ext_payment_timestamp' => $payPalData->getPaymentTimestamp(),
-				'first_payment_date' => $payPalData->getFirstPaymentDate()
-			]
-		) );
+	private function setBankDataFields( DoctrineApplication $application, array $paymentSpecificValues ): void {
+		$application->setPaymentBankAccount( $paymentSpecificValues['konto'] ?? '' );
+		$application->setPaymentBankCode( $paymentSpecificValues['blz'] ?? '' );
+		$application->setPaymentBankName( $paymentSpecificValues['bankname'] ?? '' );
+		$application->setPaymentBic( $paymentSpecificValues['bic'] ?? '' );
+		$application->setPaymentIban( $paymentSpecificValues['iban'] ?? '' );
 	}
 
 	private function getDoctrineStatus( MembershipApplication $application ): int {
--- tests/Unit/LegacyConverters/DomainToLegacyConverterTest.php
+++ tests/Unit/LegacyConverters/DomainToLegacyConverterTest.php
@@ -5,13 +5,12 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\MembershipContext\Tests\Unit\LegacyConverters;
 
 use PHPUnit\Framework\TestCase;
-use WMDE\Euro\Euro;
 use WMDE\Fundraising\MembershipContext\DataAccess\DoctrineEntities\MembershipApplication as DoctrineApplication;
 use WMDE\Fundraising\MembershipContext\DataAccess\LegacyConverters\DomainToLegacyConverter;
 use WMDE\Fundraising\MembershipContext\Domain\Model\Incentive;
 use WMDE\Fundraising\MembershipContext\Tests\Data\ValidMembershipApplication;
-use WMDE\Fundraising\PaymentContext\Domain\Model\Iban;
-use WMDE\Fundraising\PaymentContext\Domain\Model\PayPalData;
+use WMDE\Fundraising\PaymentContext\Domain\Model\LegacyPaymentData;
+use WMDE\Fundraising\PaymentContext\Domain\Model\LegacyPaymentStatus;
 
 /**
  * @covers \WMDE\Fundraising\MembershipContext\DataAccess\LegacyConverters\DomainToLegacyConverter
@@ -19,13 +18,16 @@ use WMDE\Fundraising\PaymentContext\Domain\Model\PayPalData;
 class DomainToLegacyConverterTest extends TestCase {
 
 	public function testWhenPersistingApplicationWithModerationFlag_doctrineApplicationHasFlag(): void {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
 		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 		$application->markForModeration();
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'UEB', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
 
 		$this->assertTrue( $doctrineApplication->needsModeration() );
 		$this->assertFalse( $doctrineApplication->isCancelled() );
@@ -33,13 +35,16 @@ class DomainToLegacyConverterTest extends TestCase {
 	}
 
 	public function testWhenPersistingApplicationWithCancelledFlag_doctrineApplicationHasFlag(): void {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
 		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 		$application->cancel();
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'UEB', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
 
 		$this->assertTrue( $doctrineApplication->isCancelled() );
 		$this->assertFalse( $doctrineApplication->needsModeration() );
@@ -47,117 +52,92 @@ class DomainToLegacyConverterTest extends TestCase {
 	}
 
 	public function testWhenPersistingApplicationWithConfirmedFlag_doctrineApplicationHasFlag(): void {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
 		$doctrineApplication = new DoctrineApplication();
-		// Direct debit payments are auto-confirmed
+		$application = ValidMembershipApplication::newDomainEntity();
+		$application->confirm();
+
+		$converter = new DomainToLegacyConverter();
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'UEB', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
+
+		$this->assertFalse( $doctrineApplication->isCancelled() );
+		$this->assertFalse( $doctrineApplication->needsModeration() );
+		$this->assertTrue( $doctrineApplication->isConfirmed() );
+	}
+
+	public function testWhenPersistingNonConfirmedOrModeratedOrCancelledApplication_doctrineApplicationHasFlag(): void {
+		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'UEB', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
 
-		$this->assertTrue( $doctrineApplication->isConfirmed() );
+		$this->assertFalse( $doctrineApplication->isConfirmed() );
 		$this->assertFalse( $doctrineApplication->isCancelled() );
 		$this->assertFalse( $doctrineApplication->needsModeration() );
 	}
 
 	public function testWhenPersistingCancelledModerationApplication_doctrineApplicationHasFlags(): void {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
 		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 		$application->markForModeration();
 		$application->cancel();
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'UEB', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
 
 		$this->assertTrue( $doctrineApplication->needsModeration() );
 		$this->assertTrue( $doctrineApplication->isCancelled() );
 	}
 
-	public function testWhenGivenPaypalDoctrineApplication_setsPaypalFieldsInMembershipApplication() {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
-		$paypalData = ( new PayPalData() )
-			->setPayerId( '42' )
-			->setSubscriberId( '43' )
-			->setPayerStatus( 'decent' )
-			->setAddressStatus( 'also_decent' )
-			->setAmount( Euro::newFromString( '1000' ) )
-			->setCurrencyCode( 'EUR' )
-			->setFee( Euro::newFromString( '22' ) )
-			->setSettleAmount( Euro::newFromString( '11' ) )
-			->setFirstName( 'Joe' )
-			->setLastName( 'Strummer' )
-			->setAddressName( 'Joe Strummer' )
-			->setPaymentId( '4242' )
-			->setPaymentType( 'Payment Type' )
-			->setPaymentStatus( 'all good' )
-			->setPaymentTimestamp( '1619438201' )
-			->setFirstPaymentDate( '1587902201' )
-			->freeze()->assertNoNullFields();
-
-		$doctrineApplication = new DoctrineApplication();
-		$application = ValidMembershipApplication::newDomainEntityUsingPayPal( $paypalData );
-
-		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
-		$paypalInfo = $doctrineApplication->getDecodedData();
-
-		$this->assertEquals( $paypalData->getPayerId(), $paypalInfo[ 'paypal_payer_id' ] );
-		$this->assertEquals( $paypalData->getSubscriberId(), $paypalInfo[ 'paypal_subscr_id' ] );
-		$this->assertEquals( $paypalData->getPayerStatus(), $paypalInfo[ 'paypal_payer_status' ] );
-		$this->assertEquals( $paypalData->getAddressStatus(), $paypalInfo[ 'paypal_address_status' ] );
-		$this->assertEquals( $paypalData->getAmount(), $paypalInfo[ 'paypal_mc_gross' ] );
-		$this->assertEquals( $paypalData->getCurrencyCode(), $paypalInfo[ 'paypal_mc_currency' ] );
-		$this->assertEquals( $paypalData->getFee(), $paypalInfo[ 'paypal_mc_fee' ] );
-		$this->assertEquals( $paypalData->getSettleAmount(), $paypalInfo[ 'paypal_settle_amount' ] );
-		$this->assertEquals( $paypalData->getFirstName(), $paypalInfo[ 'paypal_first_name' ] );
-		$this->assertEquals( $paypalData->getLastName(), $paypalInfo[ 'paypal_last_name' ] );
-		$this->assertEquals( $paypalData->getAddressName(), $paypalInfo[ 'paypal_address_name' ] );
-		$this->assertEquals( $paypalData->getPaymentId(), $paypalInfo[ 'ext_payment_id' ] );
-		$this->assertEquals( $paypalData->getPaymentStatus(), $paypalInfo[ 'ext_payment_status' ] );
-		$this->assertEquals( $paypalData->getPaymentTimestamp(), $paypalInfo[ 'ext_payment_timestamp' ] );
-		$this->assertEquals( $paypalData->getFirstPaymentDate(), $paypalInfo[ 'first_payment_date' ] );
-	}
-
-	public function testWhenGivenDirectDebitDoctrineApplication_setsDirectDebitFieldsInMembershipApplication() {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
+	public function testWhenGivenPayment_setsInMembershipApplication() {
 		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 
-		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
-
-		/** @var \WMDE\Fundraising\PaymentContext\Domain\Model\DirectDebitPayment $payment */
-		$payment = $application->getPayment()->getPaymentMethod();
-		$bankData = $payment->getBankData();
-
-		$this->assertEquals( $bankData->getAccount(), $doctrineApplication->getPaymentBankAccount() );
-		$this->assertEquals( $bankData->getBankCode(), $doctrineApplication->getPaymentBankCode() );
-		$this->assertEquals( $bankData->getBankName(), $doctrineApplication->getPaymentBankName() );
-		$this->assertEquals( $bankData->getBic(), $doctrineApplication->getPaymentBic() );
-		$this->assertEquals( $bankData->getIban(), new Iban( $doctrineApplication->getPaymentIban() ) );
-	}
-
-	public function testWhenGivenNonModeratedNonCancelledApplicationWithUncompletedExternalPayment_setsNeutralStatus() {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
-		$doctrineApplication = new DoctrineApplication();
-		$application = ValidMembershipApplication::newDomainEntityUsingPayPal( new PayPalData() );
+		$paymentData = new LegacyPaymentData( 1, 1, 'UEB', [
+			'konto' => 'account',
+			'blz' => 'bank code',
+			'bankname' => 'bank name',
+			'bic' => 'biccybic',
+			'iban' => 'ibannabi',
+		], LegacyPaymentStatus::EXTERNAL_BOOKED->value );
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert( $doctrineApplication, $application, $paymentData );
 
-		$this->assertEquals( DoctrineApplication::STATUS_NEUTRAL, $doctrineApplication->getStatus() );
+		$bankData = $paymentData->paymentSpecificValues;
+
+		$this->assertEquals( $application->getPaymentId(), $doctrineApplication->getPaymentId() );
+		$this->assertEquals( $bankData['konto'], $doctrineApplication->getPaymentBankAccount() );
+		$this->assertEquals( $bankData['blz'], $doctrineApplication->getPaymentBankCode() );
+		$this->assertEquals( $bankData['bankname'], $doctrineApplication->getPaymentBankName() );
+		$this->assertEquals( $bankData['bic'], $doctrineApplication->getPaymentBic() );
+		$this->assertEquals( $bankData['iban'], $doctrineApplication->getPaymentIban() );
 	}
 
 	public function testGivenApplicationWithIncentives_addsThemToDomainApplication() {
-		$this->markTestIncomplete( 'This will work again when we update the legacy converters' );
 		$incentive = new Incentive( 'PS5 and 3080 GPU and Blue Hearts album on vinyl and Analogue Pocket' );
 		$doctrineApplication = new DoctrineApplication();
 		$application = ValidMembershipApplication::newDomainEntity();
 		$application->addIncentive( $incentive );
 
 		$converter = new DomainToLegacyConverter();
-		$converter->convert( $doctrineApplication, $application );
+		$converter->convert(
+			$doctrineApplication,
+			$application,
+			new LegacyPaymentData( 1, 1, 'BEZ', [], LegacyPaymentStatus::EXTERNAL_BOOKED->value )
+		);
 
 		$doctrineIncentives = $doctrineApplication->getIncentives();
 
