da6a426b1b97081c15bf8bfcdd76038f22098529
Gabriel Birke <gabriel.birke@posteo.net>
2022-06-13 15:16:43 +0200

    Change deprecated exception in test

--- tests/Integration/DataAccess/DoctrineMembershipApplicationAuthorizerTest.php
+++ tests/Integration/DataAccess/DoctrineMembershipApplicationAuthorizerTest.php
@@ -5,7 +5,7 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\MembershipContext\Tests\Integration\DataAccess;
 
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Exception\ORMException;
 use PHPUnit\Framework\TestCase;
 use WMDE\Fundraising\MembershipContext\Authorization\ApplicationAuthorizer;
 use WMDE\Fundraising\MembershipContext\DataAccess\DoctrineApplicationAuthorizer;
