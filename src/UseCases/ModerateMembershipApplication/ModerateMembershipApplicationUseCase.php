beaa953885072eed736f9fba4b2656a9f2a11b26
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-10-25 11:20:48 +0200

    Confirm memberships when approving
    The "confirmed" state currently relies on the "completed" payment state.
    When moderating memberships, the confirmed state gets lost in the legacy
    status, so we need to explicitly confirm memberships when un-moderating (approving) them.
    
    Ticket: https://phabricator.wikimedia.org/T318217

--- src/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCase.php
+++ src/UseCases/ModerateMembershipApplication/ModerateMembershipApplicationUseCase.php
@@ -4,22 +4,23 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\MembershipContext\UseCases\ModerateMembershipApplication;
 
+use WMDE\Fundraising\MembershipContext\Domain\Model\MembershipApplication;
 use WMDE\Fundraising\MembershipContext\Domain\Model\ModerationIdentifier;
 use WMDE\Fundraising\MembershipContext\Domain\Model\ModerationReason;
 use WMDE\Fundraising\MembershipContext\Domain\Repositories\ApplicationRepository;
 use WMDE\Fundraising\MembershipContext\Infrastructure\MembershipApplicationEventLogger;
+use WMDE\Fundraising\PaymentContext\Domain\PaymentRepository;
 
 class ModerateMembershipApplicationUseCase {
 
 	public const LOG_MESSAGE_MARKED_FOR_MODERATION = 'marked for moderation by user: %s';
 	public const LOG_MESSAGE_MARKED_AS_APPROVED = 'marked as approved by user: %s';
 
-	private ApplicationRepository $applicationRepository;
-	private MembershipApplicationEventLogger $applicationEventLogger;
-
-	public function __construct( ApplicationRepository $applicationRepository, MembershipApplicationEventLogger $applicationEventLogger ) {
-		$this->applicationRepository = $applicationRepository;
-		$this->applicationEventLogger = $applicationEventLogger;
+	public function __construct(
+		private readonly ApplicationRepository $applicationRepository,
+		private readonly MembershipApplicationEventLogger $applicationEventLogger,
+		private readonly PaymentRepository $paymentRepository
+	) {
 	}
 
 	public function markMembershipApplicationAsModerated( int $membershipApplicationId, string $authorizedUser ): ModerateMembershipApplicationResponse {
@@ -60,6 +61,8 @@ class ModerateMembershipApplicationUseCase {
 		}
 
 		$membershipApplication->approve();
+		$this->confirmMembershipBasedOnPayment( $membershipApplication );
+
 		$this->applicationRepository->storeApplication( $membershipApplication );
 
 		$this->applicationEventLogger->log(
@@ -68,6 +71,13 @@ class ModerateMembershipApplicationUseCase {
 		);
 
 		return ModerateMembershipApplicationResponse::newSuccessResponse( $membershipApplicationId );
+	}
+
+	private function confirmMembershipBasedOnPayment( MembershipApplication $membershipApplication ): void {
+		$payment = $this->paymentRepository->getPaymentById( $membershipApplication->getPaymentId() );
+		if ( $payment->isCompleted() ) {
+			$membershipApplication->confirm();
+		}
 	}
 
 }
