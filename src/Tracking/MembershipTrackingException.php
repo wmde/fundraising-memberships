2e1b06cb479d5bd9ae0e41493d871a98e3b37880
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2025-01-07 18:16:10 +0100

    Fix codestyle

--- src/Tracking/MembershipTrackingException.php
+++ src/Tracking/MembershipTrackingException.php
@@ -9,7 +9,7 @@ use RuntimeException;
 
 class MembershipTrackingException extends RuntimeException {
 
-	public function __construct( string $message, Exception $previous = null ) {
+	public function __construct( string $message, ?Exception $previous = null ) {
 		parent::__construct( $message, 0, $previous );
 	}
 
