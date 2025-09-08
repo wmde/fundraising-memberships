f3c51333fa01ccf36032321d75e17a3b3e127031
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2025-09-08 16:02:10 +0200

    Add UUID to usecase method
    - the UUID is needed in the code that calls this function in the application

--- src/UseCases/FeeChange/ShowFeeChangePresenter.php
+++ src/UseCases/FeeChange/ShowFeeChangePresenter.php
@@ -5,7 +5,7 @@ declare( strict_types = 1 );
 namespace WMDE\Fundraising\MembershipContext\UseCases\FeeChange;
 
 interface ShowFeeChangePresenter {
-	public function showFeeChangeForm( int $externalMemberId, int $currentAmountInCents, int $suggestedAmountInCents, int $currentInterval ): void;
+	public function showFeeChangeForm( string $uuid, int $externalMemberId, int $currentAmountInCents, int $suggestedAmountInCents, int $currentInterval ): void;
 
 	public function showFeeChangeError(): void;
 
